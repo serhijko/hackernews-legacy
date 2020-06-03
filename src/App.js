31be8c0a8d63cc50718718795646403db8f7e056
serhijko <sp_by@tut.by>
2020-06-03 07:33:29 +0300

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -51,9 +51,6 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
-
-    if (!result) { return null; }
-
     return (
       <div className="page">
         <div className="interactions">
@@ -64,11 +61,13 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result &&
+          <Table
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        }
       </div>
     );
   }
