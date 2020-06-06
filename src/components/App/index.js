d4b5a802ed691f6115811e442eceb2e9d2cef945
serhijko <sp_by@tut.by>
2020-06-06 13:48:01 +0300

    Code Organization with ES6 Modules

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
-
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_HPP = 100;
-
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
+import './index.css';
+import Button from '../Button';
+import Table from '../Table';
+import Search from '../Search';
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP,
+} from '../../constants';
 
 class App extends Component {
   _isMonted = false;
@@ -154,63 +157,5 @@ class App extends Component {
     );
   }
 }
-
-const Search = ({
-  value,
-  onChange,
-  onSubmit,
-  children
-}) =>
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>;
-
-const Table = ({ list, onDismiss }) =>
-  <div className="table">
-    {list.map(item =>
-      <div key={item.objectID} className="table-row">
-        <span style={{ width: '40%' }}>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span style={{ width: '30%' }}>
-          {item.author}
-        </span>
-        <span style={{ width: '10%' }}>
-          {item.num_comments}
-        </span>
-        <span style={{ width: '10%' }}>
-          {item.points}
-        </span>
-        <span style={{ width: '10%' }}>
-          <Button
-            onClick={() => onDismiss(item.objectID)}
-            className="button-inline"
-          >
-            Dismiss
-          </Button>
-        </span>
-      </div>
-    )}
-  </div>;
-
-const Button = ({
-      onClick,
-      className = '',
-      children,
-}) =>
-  <button
-    onClick={onClick}
-    className={className}
-    type="button"
-  >
-    {children}
-  </button>;
 
 export default App;
