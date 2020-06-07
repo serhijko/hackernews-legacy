720993f478b6cbb56e55badd0abe11f22deca9bc
serhijko <sp_by@tut.by>
2020-06-07 16:01:42 +0300

    Unit Tests with Enzyme

--- src/components/Table/index.test.js
+++ src/components/Table/index.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Table from './index';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Table', () => {
 
@@ -23,6 +27,14 @@ describe('Table', () => {
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
   });
 
 });
